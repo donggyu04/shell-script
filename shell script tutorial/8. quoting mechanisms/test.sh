41e1a72c7cf9bd350aaf5e641f5062e4ba6ab1b7
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-28 17:55:35 +0900

    Add chapter 8 (quoting mechanisims)

---
+++ shell script tutorial/8. quoting mechanisms/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Hello\; World
+echo "I have \$1200"
+
+echo \<-\$1500.\*\*\>\; \(update\?\) \[y\|n\]
+echo '<-$1500.**>; (update?) [y|n]'
+
+VAR=ZARA
+echo "$VAR owes <-\$1500.**>; [ as of (`date +%m/%d`) ]"
+echo '$VAR owes <-\$1500.**>; [ as of (`date +%m/%d`) ]'
